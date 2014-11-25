ac2a7192330ba40b7d2ca761c797f5a906ce4e7a
Parinda Darden <parinda.darden@gmail.com>
2014-11-25 14:21:32 -0500

    ch 3 done

--- 03-routing/index.js
+++ 03-routing/index.js
@@ -3,14 +3,17 @@ var koa = require('koa');
 
 var app = module.exports = koa();
 
-app.use(function* () {
-
+app.use(function* (next) {
+  if (this.path !== '/') return yield next;
+  this.body = 'hello world';
 });
 
-app.use(function* () {
-
+app.use(function* (next) {
+  if (this.path !== '/404') return yield next;
+  this.body = 'page not found';
 });
 
-app.use(function* () {
-
+app.use(function* (next) {
+  if (this.path !== '/500') return yield next;
+  this.body = 'internal server error';
 });
