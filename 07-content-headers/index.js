13e6191ff080cceb764fa8db6922cf93c080a543
Parinda Darden <parinda.darden@gmail.com>
2014-11-25 14:49:42 -0500

    ch 7 done

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -3,6 +3,10 @@ var koa = require('koa');
 
 var app = module.exports = koa();
 
-app.use(function* () {
-  
+app.use(function* (next) {
+  if (this.request.is('application/json')) {
+    this.body = { message: 'hi!' };
+  } else {
+    this.body = 'ok';
+  }
 })
