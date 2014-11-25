544e147d53b55de0f919f578d3f5cd6fdb604b49
Parinda Darden <parinda.darden@gmail.com>
2014-11-25 14:28:37 -0500

    ch 4 done

--- 04-bodies/index.js
+++ 04-bodies/index.js
@@ -1,6 +1,7 @@
 
 var fs = require('fs');
 var koa = require('koa');
+var stat = require('../01-co/index').stat;
 
 var app = module.exports = koa();
 
@@ -12,8 +13,9 @@ var app = module.exports = koa();
 app.use(function* (next) {
   if (this.request.path !== '/stream') return yield* next;
 
-  // this.response.type =
-  // this.response.body =
+  this.response.type = 'application/javascript';
+  this.response.body = fs.createReadStream(__filename);
+  this.response.length = (yield stat(__filename)).size;
 });
 
 /**
@@ -23,5 +25,5 @@ app.use(function* (next) {
 app.use(function* (next) {
   if (this.request.path !== '/json') return yield* next;
 
-  // this.response.body =
+  this.response.body = { message: 'hello world' };
 });
